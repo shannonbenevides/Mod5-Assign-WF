3a4e31dacf85bde3073324fb72045139519a8d9c
shannonbenevides <84254255+shannonbenevides@users.noreply.github.com>
2021-08-05 06:51:56 -0400

    Add files via upload

---
+++ question.js
@@ -0,0 +1,45 @@
+
+
+ // Array of Objects
+ const quiz = [
+  {
+  	q:'Which is the color of the sky?',
+  	options:['Purple','Blue','Yellow','Black'],
+  	answer:1
+  },
+  {
+  	q:'Which of the following does NOT have four legs?',
+  	options:['Horse','Cat','Fish','Dog'],
+  	answer:2
+  },
+  {
+  	q:'Speeding is bad.',
+  	options:['true','false'],
+  	answer:0
+  },
+  {
+  	q:'What sounds does a cow make?',
+  	options:['Bawk Bawk','Meow','Moooo'],
+  	answer:2
+  },
+  {
+  	q:'How many days are in a week?',
+  	options:['54','11','36','7'],
+  	answer:3
+  },
+  {
+    q:'Where should you go potty?',
+    options:['Outside','The Sink','A Bush','The Bathroom'],
+    answer:3,
+  },
+  {
+    q:'What letter is missing from this word: sc_ool?',
+    options:['The Letter A','The Letter H','The Letter J'],
+    answer:1,
+  },
+  {
+    q:'Which of the follow is made for breakfast?',
+    options:['Pancakes','Philly Cheesesteak','Sushi','Stir Fry'],
+    answer:0,
+  }
+ ]
